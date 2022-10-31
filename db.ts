517cd2f55c34e9a11b0c4fc9fb3d57f544a59575
Brunno Manoel Azevedo Pessôa <83932059+Brunnosama@users.noreply.github.com>
2022-10-31 22:51:52 +0000

    Initial commit

---
+++ db.ts
@@ -0,0 +1,10 @@
+import { Sequelize } from 'sequelize';
+// const sequelize = new Sequelize('mysql://root:12345678@localhost:3306/projetoAdminjs', {
+//     dialect: 'mysql'
+// });
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './database.sqlite'
+})
+
+export default sequelize;
